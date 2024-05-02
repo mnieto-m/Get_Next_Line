79cfb3e6dbfde324ccf4d3ff7319c1d1ca1a94fd
mnieto-m <majanoyieto@gmail.com>
2024-05-02 17:15:08 +0200

    norminette

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: mnieto-m <mnieto-m@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/22 16:54:45 by mnieto-m          #+#    #+#             */
-/*   Updated: 2024/05/02 17:11:07 by mnieto-m         ###   ########.fr       */
+/*   Updated: 2024/05/02 17:14:42 by mnieto-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ char	*ft_read(char *buffer, int fd)
 {
 	int		bytes_read;
 	char	*tmp;
-	
+
 	tmp = 0;
 	bytes_read = read(fd, buffer, BUFFER_SIZE);
 	if (bytes_read < 0)
@@ -44,5 +44,5 @@ char	*get_next_line(int fd)
 	if (!buffer)
 		return (NULL);
 	line = ft_(buffer);
-	return(line);
+	return (line);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,24 +6,24 @@
 /*   By: mnieto-m <mnieto-m@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/22 16:55:03 by mnieto-m          #+#    #+#             */
-/*   Updated: 2024/05/02 16:14:01 by mnieto-m         ###   ########.fr       */
+/*   Updated: 2024/05/02 17:14:55 by mnieto-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-#ifndef BUFFER_SIZE
-# define BUFFER_SIZE 4
-#endif
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 4
+# endif
 
-#include <stdio.h>
-#include <unistd.h>
-#include <stdlib.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
 
 char	*get_next_line(int fd);
 char	*ft_read(char *buffer, int fd);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strdup(const char *s1);
 
-#endif 
+#endif
